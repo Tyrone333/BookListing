6f39f164f47e9f9fb23cd0e14683991bee915231
Lukas Langer <tyrone3@Lukass-MBP.home>
2016-11-18 19:02:07 +0100

    second commit

--- app/src/main/java/com/example/android/booklisting/Book.java
+++ app/src/main/java/com/example/android/booklisting/Book.java
@@ -1,10 +1,13 @@
 package com.example.android.booklisting;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by tyrone3 on 16.11.16.
  */
 
-public class Book {
+public class Book implements Parcelable{
 
     private String mTitle;
     private String mAuthor;
@@ -14,6 +17,34 @@ public class Book {
         mAuthor = author;
     }
 
+    protected Book(Parcel in) {
+        mTitle = in.readString();
+        mAuthor = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
     public String getTitle() {return mTitle;}
     public String getAuthor() {return mAuthor;}
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mTitle);
+        parcel.writeString(mAuthor);
+    }
 }
--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.booklisting;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -17,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String httpsEnd = "&maxResults=40";
     private static String searchText = "";
 
+    private TextView mEmptyStateTextView;
+    private ListView listView;
+    private Button searchButton;
+
+    BookAdapter adapter;
+    ArrayList<Book> books;
 
 
     @Override
@@ -24,57 +34,111 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button searchButton = (Button) findViewById(R.id.button_search);
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        listView = (ListView) findViewById(R.id.list);
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        searchButton = (Button) findViewById(R.id.button_search);
 
+        //Get the saved data and show it
+        if (savedInstanceState != null) {
+            books = savedInstanceState.getParcelableArrayList("key");
+            adapter = new BookAdapter(MainActivity.this, books);
+            listView.setAdapter(adapter);
+        }
+
+        //Set up the empty view
+        listView.setEmptyView(mEmptyStateTextView);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText searchEditText = (EditText) findViewById(R.id.search_edit_text);
-                searchText = searchEditText.getText().toString().trim().replace(" ", "+");
-                new bookAsyncTask().execute();
-//                Toast.makeText(MainActivity.this, https + searchText + httpsEnd, Toast.LENGTH_SHORT).show();
+                // Get a reference to the ConnectivityManager to check state of network connectivity
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                // Get details on the currently active default data network
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+                // If there is a network connection, fetch data
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    //Hide the keyboard
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+                    //Fire off the AsyncTask
+                    EditText searchEditText = (EditText) findViewById(R.id.search_edit_text);
+                    searchText = searchEditText.getText().toString().trim().replace(" ", "+");
+                    new bookAsyncTask().execute();
+
+                } else {
+                    // First, hide loading indicator so error message will be visible
+                    View loadingIndicator = findViewById(R.id.loading_indicator);
+                    loadingIndicator.setVisibility(View.GONE);
+
+                    // Clear the adapter of previous book data
+                    if (adapter != null) {
+                        adapter.clear();
+                    }
+
+                    // Update empty state with no connection error message
+                    mEmptyStateTextView.setText(R.string.no_internet_connection);
+                }
             }
         });
+    }
+
+    //Save the current books list so we dont lose our view when we rotate
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("key", books);
+        super.onSaveInstanceState(outState);
     }
 
     private class bookAsyncTask extends AsyncTask<String, Void, ArrayList<Book>> {
 
         @Override
         protected void onPreExecute() {
+            // Show loading indicator because we start to fetch data
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.VISIBLE);
 
         }
 
         @Override
         protected ArrayList<Book> doInBackground(String... urls) {
-
+            //first create the URL and then fetch the data
             urls = new String[]{https + searchText + httpsEnd};
-            ArrayList<Book> books = Utils.fetchBookList(urls[0]);
+            books = Utils.fetchBookList(urls[0]);
             return books;
         }
 
         @Override
         protected void onPostExecute(ArrayList<Book> books) {
-            if (books == null) {
-                return;
+
+            // Hide loading indicator because the data has been loaded
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Clear the adapter of previous book data
+            if (adapter != null) {
+                adapter.clear();
             }
-            updateUi(books);
 
+            // Set empty state text to display "No books found."
+            mEmptyStateTextView.setText(R.string.no_books);
+
+            if (books != null && !books.isEmpty()) {
+                updateUi(books);
+            }
         }
     }
+
+
     private void updateUi(ArrayList<Book> books) {
-        if (books.size() == 0) {
-            ListView listView = (ListView) findViewById(R.id.list);
-            listView.setAdapter(null);
-            TextView emptyText = (TextView) findViewById(R.id.empty_view);
-            emptyText.setText(R.string.empty_phrase);
-        } else {
-            TextView emptyText = (TextView) findViewById(R.id.empty_view);
-            emptyText.setText("");
-            BookAdapter adapter = new BookAdapter(MainActivity.this, books);
-            ListView listView = (ListView) findViewById(R.id.list);
-            listView.setAdapter(adapter);
-        }
 
-        }
+        adapter = new BookAdapter(MainActivity.this, books);
+        listView.setAdapter(adapter);
+
 
     }
+
+}
